ba5b572e418acbf25a06e1d858ae0a2292d45e94
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-04 10:15:51 -0400

    prueba

--- build/deploy-prod.sh
+++ build/deploy-prod.sh
@@ -3,4 +3,5 @@ echo "Converting to MDAPI format..."
 sfdx force:source:convert -r ./src/ -d deploy_prod
 #Deploy to prod & run all tests
 echo "Deploying to production & running all tests..."
-sfdx force:mdapi:deploy -u DevHub -d deploy_prod/ -w -1 -l RunAllTestsInOrg
+#sfdx force:mdapi:deploy -u DevHub -d deploy_prod/ -w -1 -l RunAllTestsInOrg
+sfdx force:mdapi:deploy -x C:/Users/karen/Documents/GitHub/amicarci/src/main/default/package.xml

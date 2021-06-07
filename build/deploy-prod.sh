21ae21379b445af6a713ae2f17afb94192bb3e43
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-07 15:39:46 -0400

    Update deploy-prod.sh

--- build/deploy-prod.sh
+++ build/deploy-prod.sh
@@ -1,7 +1,7 @@
 #Convert to MDAPI format for deployment to prod
 echo "Converting to MDAPI format..."
-sfdx force:source:deploy -r ./src/ -d deploy_prod
+sfdx force:source:convert -r ./src/ -d deploy_prod
 #Deploy to prod & run all tests
-echo "Deploying to production & running all tests..."
+echo "Deploying to Desa & running all tests..."
 sfdx force:mdapi:deploy -u Nectia -d deploy_prod/ -w -1 -l RunAllTestsInOrg
 #sfdx force:source:deploy -x C:/Users/karen/Documents/GitHub/amicarci/src/main/default/package.xml -g

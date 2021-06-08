65b383648853d0d146f10f5e6a213f68ab878e60
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-08 09:26:57 -0400

    Update deploy-prod.sh

--- build/deploy-prod.sh
+++ build/deploy-prod.sh
@@ -1,8 +1,8 @@
 #Convert to MDAPI format for deployment to prod
 echo "Converting to MDAPI format..."
-sfdx force: mdapi:convert -r mdapipackage /
+sfdx force: mdapi:convert -d deploy_prod -r mdapipackage /
 #sfdx force:source:convert -d deploy_prod -r src
 #Deploy to prod & run all tests
 echo "Deploying to Desa & running all tests..."
-sfdx force:mdapi:deploy -u amicar -d deploy_prod/ -w -1 -l RunAllTestsInOrg
+sfdx force:mdapi:deploy -u karodrilab -d deploy_prod/ -w -1 -l RunAllTestsInOrg
 #sfdx force:source:deploy -x C:/Users/karen/Documents/GitHub/amicarci/src/main/default/package.xml -g

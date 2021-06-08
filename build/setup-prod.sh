b0ccfca29dcb6da067a6fed291bc11c2df3b3d86
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-07 21:58:18 -0400

    kar

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -5,5 +5,5 @@ echo $SFDC_SERVER_KEY | base64 -d > JWT/server.key
 
 # Authenticate to salesforce
 echo "Authenticating..."
-sfdx force: auth: logout --targetusername% USERNAME% -p sfdx force:auth:jwt:grant --clientid $SFDX_CONSUMER_KEY --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
+sfdx force: auth: logout --targetusername% USERNAME% -p sfdx force:auth:jwt:grant --clientid $SFDX_CONSUMER_KEY --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a karodrilab
  
